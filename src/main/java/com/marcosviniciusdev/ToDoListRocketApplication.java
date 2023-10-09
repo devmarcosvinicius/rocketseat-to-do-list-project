db7a244769d4093efa3248a6196c99b5d012eb70
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-10-09 11:03:22 -0300

    Project generated

---
+++ src/main/java/com/marcosviniciusdev/ToDoListRocketApplication.java
@@ -0,0 +1,13 @@
+package com.marcosviniciusdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListRocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListRocketApplication.class, args);
+    }
+
+}
